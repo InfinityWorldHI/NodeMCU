512a7a5d650894bd5be9b6a1c7dcf4f3576590c3
Electronics Tech HaIs <41703435+InfinityWorldHI@users.noreply.github.com>
2019-03-09 18:02:30 +0200

    Add files via upload

--- RFID_NodeMCU/RFID_NodeMCU.ino
+++ RFID_NodeMCU/RFID_NodeMCU.ino
@@ -4,36 +4,37 @@
  * here the youtube video if you have any question put it in the comments
  * https://youtu.be/dXZiFx6RP6s
 */
-#include <SPI.h>
-#include <MFRC522.h>        //include RFID library
-#include <WiFiClient.h> 
+
 #include <ESP8266WiFi.h>     //Include Esp library
+#include <WiFiClient.h> 
 #include <ESP8266WebServer.h>
 #include <ESP8266HTTPClient.h>
+#include <SPI.h>
+#include <MFRC522.h>        //include RFID library
 
-
-#define SS_PIN D2 //RX slave select
-#define RST_PIN D1
-//#define RedLed D1
-//#define BlueLed D2
+#define SS_PIN D8 //RX slave select
+#define RST_PIN D3
+#define RedLed D1
+#define BlueLed D2
 
 MFRC522 mfrc522(SS_PIN, RST_PIN); // Create MFRC522 instance.
 
 /* Set these to your desired credentials. */
-const char *ssid = "netis";  //ENTER YOUR WIFI SETTINGS
+const char *ssid = "SSID";  //ENTER YOUR WIFI SETTINGS
 const char *password = "password";
 
 //Web/Server address to read/write from 
-const char *host = "192.168.1.3";   //IP address of server
+const char *host = "192.168.x.x";   //IP address of server
 
 String getData ,Link;
 String CardID="";
 
 void setup() {
+  delay(1000);
   Serial.begin(115200);
   SPI.begin();  // Init SPI bus
   mfrc522.PCD_Init(); // Init MFRC522 card
-  
+
   WiFi.mode(WIFI_OFF);        //Prevents reconnection issue (taking too long to connect)
   delay(1000);
   WiFi.mode(WIFI_STA);        //This line hides the viewing of ESP as wifi hotspot
@@ -55,8 +56,8 @@ void setup() {
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());  //IP address assigned to your ESP
 
-  ///pinMode(RedLed,OUTPUT);
-  ///pinMode(BlueLed,OUTPUT);
+  pinMode(RedLed,OUTPUT);
+  pinMode(BlueLed,OUTPUT);
   
 }
 
@@ -94,43 +95,43 @@ void loop() {
   HTTPClient http;    //Declare object of class HTTPClient
   
   //GET Data
-  getData = "&Pro_id=" + CardID;  //Note "?" added at front
-  Link = "http://192.168.1.3/CustomerSide/Cus-add-pro.inc.php?Tro_id=987654" + getData;
+  getData = "?CardID=" + CardID;  //Note "?" added at front
+  Link = "http://192.168.x.x/loginsystem/postdemo.php" + getData;
   
   http.begin(Link);
   
   int httpCode = http.GET();            //Send the request
   delay(10);
   String payload = http.getString();    //Get the response payload
-  Serial.println(Link);
+  
   Serial.println(httpCode);   //Print HTTP return code
   Serial.println(payload);    //Print request response payload
   Serial.println(CardID);     //Print Card ID
-  /*
+  
   if(payload == "login"){
-    //digitalWrite(RedLed,HIGH);
+    digitalWrite(RedLed,HIGH);
     Serial.println("red on");
     delay(500);  //Post Data at every 5 seconds
   }
   else if(payload == "logout"){
-    //digitalWrite(BlueLed,HIGH);
+    digitalWrite(BlueLed,HIGH);
     Serial.println("Blue on");
     delay(500);  //Post Data at every 5 seconds
   }
   else if(payload == "succesful" || payload == "Cardavailable"){
-    //digitalWrite(BlueLed,HIGH);
-    //digitalWrite(RedLed,HIGH);
+    digitalWrite(BlueLed,HIGH);
+    digitalWrite(RedLed,HIGH);
     delay(500);  
   }
   else if(payload == "NotAllow"){
     for(int j=0; j<5; j++){
-    //digitalWrite(BlueLed,HIGH);
-    //digitalWrite(RedLed,HIGH);
+    digitalWrite(BlueLed,HIGH);
+    digitalWrite(RedLed,HIGH);
     delay(200);
-    //digitalWrite(BlueLed,LOW);
-    //digitalWrite(RedLed,LOW);
+    digitalWrite(BlueLed,LOW);
+    digitalWrite(RedLed,LOW);
     }
-  }*/
+  }
   delay(500);
   
   CardID = "";
@@ -138,7 +139,7 @@ void loop() {
   Link = "";
   http.end();  //Close connection
   
-  ///digitalWrite(RedLed,LOW);
-  ///digitalWrite(BlueLed,LOW);
+  digitalWrite(RedLed,LOW);
+  digitalWrite(BlueLed,LOW);
 }
 //=======================================================================
