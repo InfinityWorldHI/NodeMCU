59dee4d925ff94dda8fc81c645b3dc074e7b6d81
Electronics Tech HaIs <41703435+InfinityWorldHI@users.noreply.github.com>
2019-05-24 11:54:51 +0300

    Update view.php

--- loginsystem/view.php
+++ loginsystem/view.php
@@ -29,6 +29,10 @@ session_start();
 <meta charset="utf-8">	
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Users Logs</title>
+<script src="https://code.jquery.com/jquery-3.3.1.js"
+        integrity="sha1256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60="
+        crossorigin="anonymous">
+</script>
 <script src="js/jquery.min.js"></script>
 <script>
   $(document).ready(function(){
